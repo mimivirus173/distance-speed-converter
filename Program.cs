f1383a365ebe2f6176fbf126926e64f991c7ddb8
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-06-14 11:33:46 +0100

    create program
    it works now

--- Program.cs
+++ Program.cs
@@ -25,24 +25,31 @@ namespace secondsConverter
             while (true)
             {
                 // Prompts the user
-                Console.Write("Input an amount of seconds: ");
-                
+                Console.Write("Input an amount of seconds: ", Console.ForegroundColor = ConsoleColor.White);
                 string input = Console.ReadLine();
 
                 // Check for valid input
                 if (float.TryParse(input, out seconds)) {
                     if (seconds > 0) {
-                        Console.WriteLine("------------------");
                         Console.WriteLine("Converting to minutes and hours...");
 
                         // Converts the seconds
-                        float hours = (seconds / 120);
+                        TimeSpan t = TimeSpan.FromSeconds( seconds );
+
+                        string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms", 
+                            t.Hours, 
+                            t.Minutes, 
+                            t.Seconds, 
+                            t.Milliseconds);
+                        
+                        Console.WriteLine(answer, Console.ForegroundColor = ConsoleColor.Green);
+                        Console.WriteLine("------------------", Console.ForegroundColor = ConsoleColor.White);
                         
                     } else {
-                        Console.WriteLine("Invalid input!");
+                        Console.WriteLine("Invalid input!", Console.ForegroundColor = ConsoleColor.Red);
                     }
                 } else {
-                    Console.WriteLine("Invalid Input!");
+                    Console.WriteLine("Invalid Input!", Console.ForegroundColor = ConsoleColor.Red);
                 }
             }
         }
